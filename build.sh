74bcc710f0df81b55ff879c4062a51def8b266bc
Michael Vaughn <michaelbt@live.com>
2018-04-12 22:41:02 +1000

    update build script & defconfig

--- build.sh
+++ build.sh
@@ -10,10 +10,11 @@ PRODUCT_OUT=$BUILD_ROOT_DIR/kernel_out
 # Default parameter
 DEVICE="dreamlte"
 TOOLCHAIN="7"
+KERNEL_DTBTOOL="./dtbTool"
 
 case $DEVICE in
-    "dreamlte") KERNEL_DEFCONFIG=dash_defconfig;;
-    "dream2lte") KERNEL_DEFCONFIG=dash2_defconfig;;
+    "dreamlte") KERNEL_DEFCONFIG=dash_defconfig; export LOCALVERSION="-DashKernel-Dream" ;;
+    "dream2lte") KERNEL_DEFCONFIG=dash2_defconfig; export LOCALVERSION="-DashKernel-Dream2" ;;
     *) die "Invalid defconfig!";
 esac 
 
@@ -31,53 +32,32 @@ DTIMG=$PRODUCT_OUT/dt.img
 
 DTBTOOL=$KERNEL_DTBTOOL
 
-FUNC_GENERATE_DEFCONFIG()
-{
-	echo ""
-        echo "=============================================="
-        echo "START : FUNC_GENERATE_DEFCONFIG"
-        echo "=============================================="
-        echo "build config="$KERNEL_DEFCONFIG ""
-        echo ""
 
+FUNC_COMPILE_KERNEL()
+{
+	echo "----------------------------------------------"
+	echo " 1. GENERATE DEFCONFIG"
+	echo " "
+	echo "build config="$KERNEL_DEFCONFIG ""
 	make -C $BUILD_KERNEL_DIR O=$BUILD_KERNEL_OUT_DIR -j$BUILD_JOB_NUMBER ARCH=arm64 \
 			CROSS_COMPILE=$BUILD_CROSS_COMPILE \
 			$KERNEL_DEFCONFIG || exit -1
-
 	cp $BUILD_KERNEL_OUT_DIR/.config $BUILD_KERNEL_DIR/arch/arm64/configs/$KERNEL_DEFCONFIG
-
-	echo ""
-	echo "================================="
-	echo "END   : FUNC_GENERATE_DEFCONFIG"
-	echo "================================="
-	echo ""
-}
-
-FUNC_GENERATE_DTB()
-{
-	echo ""
-        echo "=============================================="
-        echo "START : FUNC_GENERATE_DTB"
-        echo "=============================================="
-        echo ""
+	echo " "
+	echo " Done!"
+	echo " "
+	echo "----------------------------------------------"
+	echo " 2. GENERATE DTB"
+	echo " "
 	rm -rf $BUILD_KERNEL_OUT_DIR/arch/arm64/boot/dts
-
 	make dtbs -C $BUILD_KERNEL_DIR O=$BUILD_KERNEL_OUT_DIR -j$BUILD_JOB_NUMBER ARCH=arm64 \
 			CROSS_COMPILE=$BUILD_CROSS_COMPILE || exit -1
-	echo ""
-	echo "================================="
-	echo "END   : FUNC_GENERATE_DTB"
-	echo "================================="
-	echo ""
-}
-
-FUNC_BUILD_KERNEL()
-{
-	echo ""
-	echo "================================="
-	echo "START   : FUNC_BUILD_KERNEL"
-	echo "================================="
-	echo ""
+	echo " "
+	echo " Done!"
+	echo " "
+	echo "----------------------------------------------"
+	echo " 3. COMPILE KERNEL"
+	echo " "
 	rm $KERNEL_IMG $BUILD_KERNEL_OUT_DIR/arch/arm64/boot/Image
 	rm -rf $BUILD_KERNEL_OUT_DIR/arch/arm64/boot/dts
 
@@ -92,41 +72,24 @@ else
 fi
 
 	cp $BUILD_KERNEL_OUT_DIR/arch/arm64/boot/Image $KERNEL_IMG
-	echo "Made Kernel image: $KERNEL_IMG"
-	echo "================================="
-	echo "END   : FUNC_BUILD_KERNEL"
-	echo "================================="
-	echo ""
-}
-
-FUNC_GENERATE_DTIMG()
-{
-	echo ""
-	echo "================================="
-	echo "START   : FUNC_GENERATE_DTIMG"
-	echo "================================="
+	echo "Made Kernel image: $KERNEL_IMG , Done!"
+	echo " "
+	echo "----------------------------------------------"
+	echo " 4. GENERATE DT.IMG"
 	rm $DTIMG
-	$DTBTOOL -o $DTIMG -s 2048 -p $BUILD_KERNEL_OUT_DIR/scripts/dtc/ $BUILD_KERNEL_OUT_DIR/arch/arm64/boot/dts/exynos
+	$DTBTOOL -s 2048 -d $BUILD_KERNEL_OUT_DIR/arch/arm64/boot/dts -o $DTIMG
 	if [ -f "$DTIMG" ]; then
 		echo "Made DT image: $DTIMG"
 	fi
-	echo "================================="
-	echo "END   : FUNC_GENERATE_DTIMG"
-	echo "================================="
-	echo ""
+	echo " Done!"
 }
 
 # MAIN FUNCTION
 (
     START_TIME=`date +%s`
 
-    FUNC_GENERATE_DEFCONFIG
-    if [ "$2" = "--dt-only" ]
-    then
-        FUNC_GENERATE_DTB
-    else
-        FUNC_BUILD_KERNEL
-    fi
+    FUNC_COMPILE_KERNEL
+
     FUNC_GENERATE_DTIMG
 
     END_TIME=`date +%s`
